a5f0dbe8d1c861daf859bb3bc2ac56accb08b84d
thomashewett <thomashewett@hotmail.com>
2020-10-21 13:29:43 +1100

    Added some cards using react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <CardList robots={robots} />
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
